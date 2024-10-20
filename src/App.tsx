17e3a37cc6f777477ec0013cdfd35bb521a94207
GAjay <devakm95@gmail.com>
2024-10-20 13:23:13 +0530

    added videos

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Navbar from './Nav'
+import Carousel from './Carousel'
 
 function App() {
-  const [count, setCount] = useState(1)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Navbar/>
+    <Carousel/>
+    <div className="elfsight-app-e6f0efec-c489-483f-a64b-d9d08e0be195" data-elfsight-app-lazy></div>
+</>
   )
 }
 
