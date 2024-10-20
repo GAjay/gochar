14b0dc79cf9c1175052bdd883d5eb2c418480dba
GAjay <devakm95@gmail.com>
2024-10-20 15:42:52 +0530

    all layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,86 +1,73 @@
 import Navbar from './Nav'
 import Carousel from './Carousel'
+import Footer from './Footer'
+
+const images=[
+  "https://scontent.fudr1-1.fna.fbcdn.net/v/t39.30808-6/441507151_122147876834193339_6759347503756173077_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=cc71e4&_nc_ohc=LNmaTDxgyJwQ7kNvgES4H3B&_nc_zt=23&_nc_ht=scontent.fudr1-1.fna&_nc_gid=AFxDCsrfN5uTL8duF1v7ZaS&oh=00_AYDkCmm8qunBMw9GrUJ4rxlE8BgZXfiVQg4s81gixPYssw&oe=671A8FE3",
+  "https://scontent.fudr1-1.fna.fbcdn.net/v/t39.30808-6/423692390_122107104500193339_7489324886397860538_n.jpg?stp=c546.0.957.957a_dst-jpg_s552x414&_nc_cat=103&ccb=1-7&_nc_sid=50c75d&_nc_ohc=vcDpm28tWiEQ7kNvgFiyGs6&_nc_zt=23&_nc_ht=scontent.fudr1-1.fna&_nc_gid=AzQqpzRwUTH5GLkFbHsp3m7&oh=00_AYBEKjIxBTjdS6XRJoZDedwHldtpCaoknQvtG5RZPAQziQ&oe=671AA008",
+  "https://scontent.fudr1-1.fna.fbcdn.net/v/t39.30808-6/428665696_122119885004193339_982216926098623815_n.jpg?stp=c546.0.957.957a_dst-jpg_s552x414&_nc_cat=105&ccb=1-7&_nc_sid=50ad20&_nc_ohc=wb2mp1vOIc4Q7kNvgEZc4fj&_nc_zt=23&_nc_ht=scontent.fudr1-1.fna&_nc_gid=ATLu50bPEOCqdgZa-f4YFb9&oh=00_AYDVLUXF8NIt5KWHpp0AHXU0qtIdw9tQNP2OuZbxGbZtbA&oe=671AA90A",
+  "https://scontent.fudr1-1.fna.fbcdn.net/v/t39.30808-6/421926230_7052903871454825_1120990758253962783_n.jpg?stp=c799.0.1399.1399a_dst-jpg_s552x414&_nc_cat=102&ccb=1-7&_nc_sid=50c75d&_nc_ohc=zonbOP9esjMQ7kNvgHOTSn4&_nc_zt=23&_nc_ht=scontent.fudr1-1.fna&_nc_gid=AzQqpzRwUTH5GLkFbHsp3m7&oh=00_AYDgIsr9YkhJn9O4Zz33Iq7OFSSdNs97xz5fIQEb1xxKpw&oe=671A8BA1",
+  "https://scontent.fudr1-1.fna.fbcdn.net/v/t39.30808-6/423247961_122108729486193339_5473014471759984058_n.jpg?stp=dst-jpg_s552x414&_nc_cat=103&ccb=1-7&_nc_sid=fe5ecc&_nc_ohc=9ED1SMAJONoQ7kNvgFxj2CC&_nc_zt=23&_nc_ht=scontent.fudr1-1.fna&_nc_gid=AzQqpzRwUTH5GLkFbHsp3m7&oh=00_AYA1CtOFr4iabGxpcbMMhXvpFhw0D8gyZJd_2-QK7M7Ajw&oe=671AA9A0",
+  "https://scontent.fudr1-1.fna.fbcdn.net/v/t39.30808-6/420127750_7052899694788576_6857767908725407799_n.jpg?stp=c799.0.1399.1399a_dst-jpg_s552x414&_nc_cat=111&ccb=1-7&_nc_sid=7a06f5&_nc_ohc=5BtIWcANuPcQ7kNvgEO3ZW3&_nc_zt=23&_nc_ht=scontent.fudr1-1.fna&_nc_gid=AzQqpzRwUTH5GLkFbHsp3m7&oh=00_AYBA8obUfjyiaZ8_sFhmv9dl__rqZrfD287CTjmb9ATp5g&oe=671ABDC6"
+]
 
 function App() {
 
   return (
-    <>
-    
-    <div className="h-svh bg-cover bg-center bg-no-repeat bg-cow">
-      <Navbar/>
-      <Carousel/>
+  <>
+      <div className="h-svh bg-cover bg-center bg-no-repeat bg-cow">
+          <Navbar/>
+          <Carousel/>
       </div>
-      <div className='mx-auto lg:px-8 z-50 relative mx-auto max-w-7xl'>
-      <div className="text-center "> <h1 className="mt-4 text-3xl tracking-tight text-gray-900 sm:text-5xl text-primary typing">
-            Anand
-              <span className="relative whitespace-nowrap text-orange-400">
-                <svg
-                  aria-hidden="true"
-                  viewBox="0 0 418 42"
-                  className="absolute top-2/3 left-0 h-[0.58em] w-full fill-orange-300/70"
-                  preserveAspectRatio="none"
-                >
-                  <path d="M203.371.916c-26.013-2.078-76.686 1.963-124.73 9.946L67.3 12.749C35.421 18.062 18.2 21.766 6.004 25.934 1.244 27.561.828 27.778.874 28.61c.07 1.214.828 1.121 9.595-1.176 9.072-2.377 17.15-3.92 39.246-7.496C123.565 7.986 157.869 4.492 195.942 5.046c7.461.108 19.25 1.696 19.17 2.582-.107 1.183-7.874 4.31-25.75 10.366-21.992 7.45-35.43 12.534-36.701 13.884-2.173 2.308-.202 4.407 4.442 4.734 2.654.187 3.263.157 15.593-.78 35.401-2.686 57.944-3.488 88.365-3.143 46.327.526 75.721 2.23 130.788 7.584 19.787 1.924 20.814 1.98 24.557 1.332l.066-.011c1.201-.203 1.53-1.825.399-2.335-2.911-1.31-4.893-1.604-22.048-3.261-57.509-5.556-87.871-7.36-132.059-7.842-23.239-.254-33.617-.116-50.627.674-11.629.54-42.371 2.494-46.696 2.967-2.359.259 8.133-3.625 26.504-9.81 23.239-7.825 27.934-10.149 28.304-14.005.417-4.348-3.529-6-16.878-7.066Z"></path>
-                </svg>
-                <span className="relative"> Videos</span>
-              </span>
-            </h1>
-            </div>
-      <div className="elfsight-app-e6f0efec-c489-483f-a64b-d9d08e0be195" data-elfsight-app-lazy></div></div>
-      <section className="mt-10 mb-20 relative overflow-hidden h-screen">
-        <div className="bg-gray">
-          <div className="text-center ">
-            <h1 className="mt-4 text-3xl tracking-tight text-gray-900 sm:text-5xl text-primary typing">
-              Glimpse Of
-              <span className="relative whitespace-nowrap text-orange-400">
-                <svg
-                  aria-hidden="true"
-                  viewBox="0 0 418 42"
-                  className="absolute top-2/3 left-0 h-[0.58em] w-full fill-orange-300/70"
-                  preserveAspectRatio="none"
-                >
-                  <path d="M203.371.916c-26.013-2.078-76.686 1.963-124.73 9.946L67.3 12.749C35.421 18.062 18.2 21.766 6.004 25.934 1.244 27.561.828 27.778.874 28.61c.07 1.214.828 1.121 9.595-1.176 9.072-2.377 17.15-3.92 39.246-7.496C123.565 7.986 157.869 4.492 195.942 5.046c7.461.108 19.25 1.696 19.17 2.582-.107 1.183-7.874 4.31-25.75 10.366-21.992 7.45-35.43 12.534-36.701 13.884-2.173 2.308-.202 4.407 4.442 4.734 2.654.187 3.263.157 15.593-.78 35.401-2.686 57.944-3.488 88.365-3.143 46.327.526 75.721 2.23 130.788 7.584 19.787 1.924 20.814 1.98 24.557 1.332l.066-.011c1.201-.203 1.53-1.825.399-2.335-2.911-1.31-4.893-1.604-22.048-3.261-57.509-5.556-87.871-7.36-132.059-7.842-23.239-.254-33.617-.116-50.627.674-11.629.54-42.371 2.494-46.696 2.967-2.359.259 8.133-3.625 26.504-9.81 23.239-7.825 27.934-10.149 28.304-14.005.417-4.348-3.529-6-16.878-7.066Z"></path>
-                </svg>
-                <span className="relative"> Anand</span>
-              </span>
-            </h1>
+        <main>
+      <section className='mt-10 mb-20 relative overflow-hidden h-screen'>
+          <div className="text-center "> 
+              <h1 className="mt-4 text-3xl tracking-tight text-gray-900 sm:text-5xl text-primary">
+                    Anand
+                      <span className="relative whitespace-nowrap text-orange-400">
+                        <svg
+                          aria-hidden="true"
+                          viewBox="0 0 418 42"
+                          className="absolute top-2/3 left-0 h-[0.58em] w-full fill-orange-300/70"
+                          preserveAspectRatio="none"
+                        >
+                          <path d="M203.371.916c-26.013-2.078-76.686 1.963-124.73 9.946L67.3 12.749C35.421 18.062 18.2 21.766 6.004 25.934 1.244 27.561.828 27.778.874 28.61c.07 1.214.828 1.121 9.595-1.176 9.072-2.377 17.15-3.92 39.246-7.496C123.565 7.986 157.869 4.492 195.942 5.046c7.461.108 19.25 1.696 19.17 2.582-.107 1.183-7.874 4.31-25.75 10.366-21.992 7.45-35.43 12.534-36.701 13.884-2.173 2.308-.202 4.407 4.442 4.734 2.654.187 3.263.157 15.593-.78 35.401-2.686 57.944-3.488 88.365-3.143 46.327.526 75.721 2.23 130.788 7.584 19.787 1.924 20.814 1.98 24.557 1.332l.066-.011c1.201-.203 1.53-1.825.399-2.335-2.911-1.31-4.893-1.604-22.048-3.261-57.509-5.556-87.871-7.36-132.059-7.842-23.239-.254-33.617-.116-50.627.674-11.629.54-42.371 2.494-46.696 2.967-2.359.259 8.133-3.625 26.504-9.81 23.239-7.825 27.934-10.149 28.304-14.005.417-4.348-3.529-6-16.878-7.066Z"></path>
+                        </svg>
+                        <span className="relative"> Videos</span>
+                      </span>
+                </h1>
           </div>
-          <div className="pt-6">
-            <div className="mx-auto mt-6 max-w-2xl sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:gap-x-8 lg:px-8">
-              <div className="aspect-h-4 aspect-w-3 overflow-hidden rounded-lg lg:block">
-                <img
-                  src="https://scontent.fudr1-1.fna.fbcdn.net/v/t39.30808-6/441507151_122147876834193339_6759347503756173077_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=5f2048&_nc_ohc=azUqU0_TPmYQ7kNvgH5oBYP&_nc_ht=scontent.fudr1-1.fna&oh=00_AYDufDgFXB9DZWj0wjhntD08UVmBR81R6jFmKZo8kFnQCQ&oe=666C89E3"
-                  alt="Two each of gray, white, and black shirts laying flat."
-                  className="h-full w-full object-cover object-center"
-                />
-              </div>
-              <div className="lg:grid lg:grid-cols-1 lg:gap-y-8">
-                <div className="aspect-h-2 aspect-w-3 overflow-hidden rounded-lg">
-                  <img
-                    src="https://scontent.fudr1-1.fna.fbcdn.net/v/t39.30808-6/423247961_122108729486193339_5473014471759984058_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=5f2048&_nc_ohc=RywHx4w5dwQQ7kNvgGJb7ts&_nc_ht=scontent.fudr1-1.fna&oh=00_AYAU--ZR9JQWrTVSil3FxfV9y88gNX1-5pP2XIGy9HUBSw&oe=666CA3A0"
-                    alt="Model wearing plain black basic tee."
-                    className="h-full w-full object-cover object-center"
-                  />
-                </div>
-                <div className="aspect-h-2 aspect-w-3 overflow-hidden rounded-lg">
-                  <img
-                    src="https://scontent.fudr1-1.fna.fbcdn.net/v/t39.30808-6/423692390_122107104500193339_7489324886397860538_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=5f2048&_nc_ohc=W6W2kYk-a8gQ7kNvgHiUdIo&_nc_ht=scontent.fudr1-1.fna&oh=00_AYCds33xAwyHseFh3ucUAr3j8UczQMMET_3puWxAGZXq_g&oe=666C9A08"
-                    alt="Model wearing plain gray basic tee."
-                    className="h-full w-full object-cover object-center"
-                  />
-                </div>
-              </div>
-              <div className="aspect-h-5 aspect-w-4 lg:aspect-h-4 lg:aspect-w-3 sm:overflow-hidden sm:rounded-lg">
-                <img
-                  src="https://scontent.fudr1-1.fna.fbcdn.net/v/t39.30808-6/428665696_122119885004193339_982216926098623815_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=5f2048&_nc_ohc=oGXhFIgxCdUQ7kNvgFujTkj&_nc_ht=scontent.fudr1-1.fna&oh=00_AYBGjT23eAaW6pqLJ808roQmYGZImPS4Lzk0ntXTHnU72g&oe=666CA30A"
-                  alt="Model wearing plain white basic tee."
-                  className="h-full w-full object-cover object-center"
-                />
-              </div>
-            </div>
-          </div>
-        </div>
+          <div className="elfsight-app-e6f0efec-c489-483f-a64b-d9d08e0be195 pt-6" data-elfsight-app-lazy/>
       </section>
+      <section className="mt-10 mb-20 relative overflow-hidden h-screen">
+            <div className="text-center ">
+              <h1 className="mt-4 text-3xl tracking-tight text-gray-900 sm:text-5xl text-primary">
+                Glimpse Of
+                <span className="relative whitespace-nowrap text-orange-400">
+                  <svg
+                    aria-hidden="true"
+                    viewBox="0 0 418 42"
+                    className="absolute top-2/3 left-0 h-[0.58em] w-full fill-orange-300/70"
+                    preserveAspectRatio="none"
+                  >
+                    <path d="M203.371.916c-26.013-2.078-76.686 1.963-124.73 9.946L67.3 12.749C35.421 18.062 18.2 21.766 6.004 25.934 1.244 27.561.828 27.778.874 28.61c.07 1.214.828 1.121 9.595-1.176 9.072-2.377 17.15-3.92 39.246-7.496C123.565 7.986 157.869 4.492 195.942 5.046c7.461.108 19.25 1.696 19.17 2.582-.107 1.183-7.874 4.31-25.75 10.366-21.992 7.45-35.43 12.534-36.701 13.884-2.173 2.308-.202 4.407 4.442 4.734 2.654.187 3.263.157 15.593-.78 35.401-2.686 57.944-3.488 88.365-3.143 46.327.526 75.721 2.23 130.788 7.584 19.787 1.924 20.814 1.98 24.557 1.332l.066-.011c1.201-.203 1.53-1.825.399-2.335-2.911-1.31-4.893-1.604-22.048-3.261-57.509-5.556-87.871-7.36-132.059-7.842-23.239-.254-33.617-.116-50.627.674-11.629.54-42.371 2.494-46.696 2.967-2.359.259 8.133-3.625 26.504-9.81 23.239-7.825 27.934-10.149 28.304-14.005.417-4.348-3.529-6-16.878-7.066Z"></path>
+                  </svg>
+                  <span className="relative"> Anand</span>
+                </span>
+              </h1>
+            </div>
+            <div className="pt-6 mt-10">
+              <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
+                {images.map((item)=>
+                  <div>
+                      <img className="h-full max-w-full rounded-lg object-cover" src={item} alt=""/>
+                  </div>)}
+                 
+              </div>
+            </div>
+      </section>
+    </main>
+    <Footer/>
     </>
   )
 }
---
+++ src/Footer.tsx
@@ -0,0 +1,39 @@
+import logo from "./assets/logo.svg";
+
+const Footer = () => (
+    <footer className="w-full py-14 border-t border-gray-200">
+    <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <div className="max-w-3xl mx-auto">
+            <a href="https://pagedone.io/" className="flex justify-center ">
+                <img src={logo} alt="logo"/>
+            </a>
+            
+                <ul className="text-lg flex items-center justify-center flex-col gap-7 md:flex-row md:gap-12 transition-all duration-500 py-16 mb-10 border-b border-gray-200">
+                    <li><a href="#about-us" className="text-gray-800 hover:text-gray-900">About Us</a></li>
+                    <li><a href="#videos" className=" text-gray-800 hover:text-gray-900">Videos</a></li>
+                    <li><a href="https://www.youtube.com/@SarehNathaniaGochar" className=" text-gray-800 hover:text-gray-900">YouTube</a></li>
+                </ul>
+                <div className="flex space-x-10 justify-center items-center mb-14">
+                    <a href="https://www.facebook.com/people/सरेह-नथानिया-गौचर-बीकानेर/61555800189887/" className="block  text-gray-900 transition-all duration-500 hover:text-indigo-600 ">
+                        <svg className="w-[0.938rem] h-[1.625rem]" viewBox="0 0 15 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M13.7926 14.4697L14.5174 9.86889H10.0528V6.87836C10.0528 5.62033 10.6761 4.39105 12.6692 4.39105H14.7275V0.473179C13.5289 0.282204 12.3177 0.178886 11.1037 0.164062C7.42917 0.164062 5.0302 2.37101 5.0302 6.36077V9.86889H0.957031V14.4697H5.0302V25.5979H10.0528V14.4697H13.7926Z" fill="currentColor"/>
+                            </svg>
+                            
+                    </a>
+                    <a href="https://www.youtube.com/@SarehNathaniaGochar" className="block  text-gray-900 transition-all duration-500 hover:text-red-600 ">
+                        <svg className="w-[1.875rem] h-[1.375rem]" viewBox="0 0 30 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M26.3106 1.27838C27.5782 1.62071 28.5745 2.61957 28.9113 3.88573C29.524 6.18356 29.524 10.9809 29.524 10.9809C29.524 10.9809 29.524 15.7782 28.9113 18.076C28.5698 19.3469 27.5735 20.3457 26.3106 20.6834C24.0186 21.2977 14.8226 21.2977 14.8226 21.2977C14.8226 21.2977 5.63122 21.2977 3.33456 20.6834C2.06695 20.3411 1.07063 19.3422 0.73385 18.076C0.121094 15.7782 0.121094 10.9809 0.121094 10.9809C0.121094 10.9809 0.121094 6.18356 0.73385 3.88573C1.07531 2.61488 2.07162 1.61602 3.33456 1.27838C5.63122 0.664062 14.8226 0.664062 14.8226 0.664062C14.8226 0.664062 24.0186 0.664062 26.3106 1.27838ZM19.5234 10.9809L11.885 15.403V6.55872L19.5234 10.9809Z" fill="currentColor"/>
+                            </svg>
+                            
+                    </a>
+                </div>
+        </div>
+      
+    </div>
+    <div className="text-lg text-white text-center block bg-orange py-5">©<a href="/">Sareh Nathania Gochar</a> {new Date().getFullYear()}, All rights reserved.</div>
+       
+</footer>
+                                        
+  );
+  
+  export default Footer
